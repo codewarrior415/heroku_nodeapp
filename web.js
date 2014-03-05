d4685144d93b7cc57631301c3e00515eb3948bf4
Matt Murillo <mattmurillo@gmail.com>
2014-03-04 22:35:44 -0800

    include ember appkit

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+//var errorLogger = new logfmt;
 
 app.use(logfmt.requestLogger());
 
@@ -8,7 +9,15 @@ app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
+logfmt.log({dir: __dirname});
+
+app.use("/vendor", express.static(__dirname + '/ember-app-kit-master/vendor'));
+app.use("/config", express.static(__dirname + '/ember-app-kit-master/config'));
+app.use("/assets", express.static(__dirname + '/ember-app-kit-master/dist/assets'));
+app.use("/app", express.static(__dirname + '/ember-app-kit-master/tmp/result'));
+app.use("/dist", express.static(__dirname + '/ember-app-kit-master/dist'));
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
